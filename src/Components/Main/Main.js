c9315c10a68674eaee1ccbf0056e830b9a897515
Rojo-Nanteh☀️ <rojo.rak39@gmail.com>
2025-04-15 06:40:00 +0300

    feat: adding the section accueil

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+
+// Images
+import arrow from './../../Assets/Icons/Right-up.png';
+
+// CSS
+import './Accueil.css'
 
 export default function Main() {
   return (
     <div>
-        <section id="section1" style={{height: '100vh', backgroundColor: '#16243D'}}></section>
-        <section id="section2" style={{height: '100vh', backgroundColor: 'black'}}></section>
+        <section id="Accueil" style={{backgroundColor: '#16243D'}}>
+          <p className='bienvenue'>Bienvenue chez <span id="logoKoti"></span><span id="logoKota"></span></p>
+          <p className="presentation">Votre Partenaire en Assistance et Support Client</p>
+          <p className="description">Chez nous, nous aidons les entreprises à offrir un <span className="orange-bold">service client</span> exceptionnel, une <span className="orange-bold">assistance virtuelle</span> efficace et une <span className="orange-bold">gestion de SAV externalisée</span> fiable.
+          Nous accompagnons les e-commerçants, boutiques en ligne, marques et entrepreneurs en prenant en charge la relation client et le support technique, afin qu’ils puissent se concentrer sur leur croissance.</p>
+          <div className="buttons">
+            <a href="#Services" className='button'>En savoir plus <img src={arrow} alt="->" /></a>
+            <a href="#Contact" className="button">Nous contacter</a>
+          </div>
+        </section>
+        <section id="Services" style={{height: '100vh', backgroundColor: 'black'}}></section>
     </div>
   )
 }
